76be500c2da1a4f249800a42ee7994059d5d34d3
Mohd Farman <ahmadfarman787@gmail.com>
2025-11-27 10:41:44 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: 'http://127.0.0.1:3000/frontend/index.html', 
+    origin: 'http://127.0.0.1:3000/', 
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
 };
